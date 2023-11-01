799088220bee4b7b998fd2b92523817ab8e3088b
tytmyt <130250967+tytmyt@users.noreply.github.com>
2023-11-01 11:32:44 +0200

    Add files via upload

---
+++ MyTaskPlanner/MyTaskPlanner.Domain.Logic/SimpleTaskPlanner.cs
@@ -0,0 +1,40 @@
+﻿using MyTaskPlanner.Domain.Models.Models;
+
+namespace MyTaskPlanner.Domain.Logic
+{
+    public static class SimpleTaskPlanner
+    {
+        public static WorkItem[] CreatePlan(WorkItem[] workItems)
+        {
+            var items = workItems.ToList();
+
+            items.Sort(CompareWorkItems);
+            return items.ToArray();
+
+        }
+
+        private static int CompareWorkItems(WorkItem firstItem, WorkItem secondItem)
+        {
+            // Спершу порівнюємо за пріоритетом (спадання).
+            int priorityComparison = secondItem.Priority.CompareTo(firstItem.Priority);
+
+            // Якщо пріоритети різні, повертаємо результат порівняння пріоритетів.
+            if (priorityComparison != 0)
+            {
+                return priorityComparison;
+            }
+
+            // Якщо пріоритети однакові, порівнюємо за DueDate (зростання).
+            int dueDateComparison = firstItem.DueDate.CompareTo(secondItem.DueDate);
+
+            // Якщо дати різні, повертаємо результат порівняння дат.
+            if (dueDateComparison != 0)
+            {
+                return dueDateComparison;
+            }
+
+            // Якщо дати однакові, порівнюємо за назвою (алфавітний порядок).
+            return firstItem.Title.CompareTo(secondItem.Title);
+        }
+    }
+}
---
+++ MyTaskPlanner/MyTaskPlanner/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using MyTaskPlanner.Domain.Models.Enums;
+using MyTaskPlanner.Domain.Models.Models;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть кількість :");
+        int amount = int.Parse(Console.ReadLine());
+
+        WorkItem[] items = new WorkItem[amount];
+
+        for (int i = 0; i < amount; i++)
+            items[i] = CreateEvent();
+    }
+
+    public static WorkItem CreateEvent()
+    {
+        try
+        {
+            WorkItem workItem = new WorkItem();
+            Console.WriteLine("Title:");
+            workItem.Title = Console.ReadLine();
+
+            workItem.CreationDate = DateTime.Now;
+            workItem.IsCompleted = false;
+
+            while (true)
+            {
+                Console.WriteLine("Due Date (yyyy-MM-dd):");
+                if (DateTime.TryParse(Console.ReadLine(), out DateTime dueDate))
+                {
+                    workItem.DueDate = dueDate;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некоректний формат дати. Спробуйте ще раз.");
+                }
+            }
+
+            Console.WriteLine("Description");
+            workItem.Description = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("Complexity (0 - Simple, 1 - Moderate, 2 - Complex):");
+                if (Enum.TryParse<Complexity>(Console.ReadLine(), out Complexity complexity))
+                {
+                    workItem.Complexity = complexity;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некоректне значення складності. Спробуйте ще раз.");
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Priority (0 - Low, 1 - Medium, 2 - High):");
+                if (Enum.TryParse<Priority>(Console.ReadLine(), out Priority priority))
+                {
+                    workItem.Priority = priority;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некоректне значення пріоритету. Спробуйте ще раз.");
+                }
+            }
+
+            return workItem;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Помилка введення!");
+            throw;
+        }
+
+    }
+}
